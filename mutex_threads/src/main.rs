2ea46b029d3116b9dadc577722553832139df85f
Simon Tharby <jinjamail@gmail.com>
2020-08-30 17:36:31 +0100

    mutex threads

--- mutex_threads/src/main.rs
+++ mutex_threads/src/main.rs
@@ -4,6 +4,10 @@
     // When you’re done with the data that the mutex guards, you must unlock the
     // data so other threads can acquire the lock.
 
+// Mutex<T> comes with the risk of creating deadlocks. These occur when an
+// operation needs to lock two resources and two threads have each acquired one
+// of the locks, causing them to wait for each other forever.
+
 // Arc<T> is a type like Rc<T> that is safe to use in concurrent situations.
 // The a stands for atomic, meaning it’s an atomically reference counted type.
 
