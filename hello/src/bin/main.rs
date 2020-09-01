3e9a7cd4917deed635d848d752776574b9202818
Simon Tharby <jinjamail@gmail.com>
2020-09-01 21:17:56 +0100

    hello server

--- hello/src/bin/main.rs
+++ hello/src/bin/main.rs
@@ -8,6 +8,7 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     let pool = ThreadPool::new(4);
 
+    // for stream in listener.incoming().take(2) { // shuts down gracefully after 2 requests
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
@@ -15,6 +16,8 @@ fn main() {
             handle_connection(stream);
         });
     }
+
+    println!("Shutting down.");
 }
 
 fn handle_connection(mut stream: TcpStream) {
