6ef0e8003ca76680f3ab837b33cfdc8483bde147
Simon Tharby <jinjamail@gmail.com>
2020-12-13 14:25:25 +0000

    Take a search phrase from args

--- tailit/src/main.rs
+++ tailit/src/main.rs
@@ -8,9 +8,12 @@ use colored::*;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
+    // let args_n: usize = args.iter().count();
+
     check_args(&args);
 
     let filename: &str = &args[1];
+    let phrase: &str = &args[2];
     let mut lines: usize = linecount(filename);
 
     loop {
@@ -22,18 +25,16 @@ fn main() {
 
             // print lines from newline vec in reverse order == order in original file
             for i in 0..num_newlines {
-                let line = &newlines[num_newlines - i - 1];
-                if line.contains("Rendered") {
-                    let line = line.replace("Rendered", "*#*Rendered*#*");
+                let raw_line = &newlines[num_newlines - i - 1];
+                if raw_line.contains(phrase) {
+                    let line = raw_line.replace(phrase, &("*#~".to_owned() + phrase + "*#~"));
+                    let split: Vec<&str> = line.split("*#~").collect();
 
-                    let split = line.split("*#*");
-                    let vec: Vec<&str> = split.collect();
-
-                    for word in vec {
-                        if word == "Rendered" {
-                            print!("{}", word.blue().bold());
+                    for p in split {
+                        if p == phrase {
+                            print!("{}", p.bright_blue().bold());
                         } else {
-                            print!("{}", word);
+                            print!("{}", p);
                         }     
                     }
                     
@@ -87,22 +88,22 @@ fn check_args(args: &Vec<String>) {
 fn print_help() {
     println!("Help - How to use tailit:\n");
     println!("16 basic colors:");
-    println!("  1. {}", "black".black());
-    println!("  2. {}", "red".red());
-    println!("  3. {}", "green".green());
-    println!("  4. {}", "yellow".yellow());
-    println!("  5. {}", "blue".blue());
-    println!("  6. {}", "magenta".magenta());
-    println!("  7. {}", "cyan".cyan());
-    println!("  8. {}", "white".white());
-    println!("  9. {}", "bright_black".bright_black());
-    println!(" 10. {}", "bright_red".bright_red());
-    println!(" 11. {}", "bright_green".bright_green());
-    println!(" 12. {}", "bright_yellow".bright_yellow());
-    println!(" 13. {}", "bright_blue".bright_blue());
-    println!(" 14. {}", "bright_magenta".bright_magenta());
-    println!(" 15. {}", "bright_cyan".bright_cyan());
-    println!(" 16. {}", "bright_white".bright_white());
+    print!("  1. {}", "black".black());
+    print!("          2. {}", "red".red());
+    print!("             3. {}", "green".green());
+    println!("         4. {}", "yellow".yellow());
+    print!("  5. {}", "blue".blue());
+    print!("           6. {}", "magenta".magenta());
+    print!("         7. {}", "cyan".cyan());
+    println!("          8. {}", "white".white());
+    print!("  9. {}", "bright_black".bright_black());
+    print!("  10. {}", "bright_red".bright_red());
+    print!("     11. {}", "bright_green".bright_green());
+    println!("  12. {}", "bright_yellow".bright_yellow());
+    print!(" 13. {}", "bright_blue".bright_blue());
+    print!("   14. {}", "bright_magenta".bright_magenta());
+    print!(" 15. {}", "bright_cyan".bright_cyan());
+    println!("   16. {}", "bright_white".bright_white());
 }
 
 // Todo:
@@ -111,4 +112,11 @@ fn print_help() {
 // Get words from args
 // Get words from 'example|another' type args
 // Get options from args (in form -example)
+// Options:
+//   color (a number) - default is bright cyan, use color value 0 for default terminal text color.
+//   regular (default is bold)
+//   lines before (if exist)
+//   lines after (if exist)
 // Add details, examples and limitations to help
+// ? Use codegen to generate code to style word(s) from option(s) args?
+//   https://docs.rs/codegen/0.1.3/codegen/
